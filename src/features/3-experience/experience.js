866c31499b5aae939900891294bed0f32b5e09e5
mohamed adel <mahmedadel973@gmail.com>
2025-11-04 12:35:00 +0200

    add experience

--- src/features/3-experience/experience.js
+++ src/features/3-experience/experience.js
@@ -5,6 +5,32 @@ const experiences = [
         period: 'Jul 2025 - Aug 2025',
         tasks: [ 'Completed an intensive one-month Flutter training program focused on mobile app development.', 'Learned Dart programming language and mastered Flutter framework fundamentals.', 'Built practical apps integrating RESTful APIs and managing state with BLoC and Cubit.', 'Gained experience in UI/UX design, responsive layouts, and clean project structure.', 'Applied best practices for app architecture and performance optimization' ]
     },
+    {
+    company: 'Google Developer Student Clubs (GDSC) - Benha University',
+    role: 'Flutter Trainee (Bootcamp)',
+    period: 'Jan 28, 2024 - Feb 1, 2024',
+    tasks: [
+      'Completed an intensive 36-hour training bootcamp focused on Flutter mobile app development fundamentals.',
+      'Developed interactive and engaging User Interfaces (UI) using various Flutter Widgets.',
+      'Applied Object-Oriented Programming (OOP) principles for effective code structure and organization.',
+      'Built a complete practical project (Calculator App) to solidify core concepts.',
+      'Gained practical skills in problem-solving and effective teamwork.'
+    ]
+},
+{
+    company: 'D.Pear',
+    role: 'Flutter Summer Trainee',
+    period: 'Summer 2024 (6 Weeks)',
+    tasks: [
+      'Gained extensive hands-on experience with core and advanced Flutter widgets.',
+      'Developed and delivered multiple projects, including a BMI Calculator and a Food App UI and bookly app with api integration.',    
+      'Integrated RESTful APIs for fetching live data and Firebase for backend services.',
+      'Implemented local data persistence using the Hive database.',
+      'Learned and applied the foundational principles of Clean Architecture for scalable project structure.',
+      'Utilized various Flutter packages, such as image_picker, to add native device functionality.',
+      'Demonstrated strong performance and collaboration skills in team-based projects.'
+    ]
+}
     
 ];
 
